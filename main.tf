b590f78cc8c7ae8fc6aee0228921e46a64c935d0
orasenatdpltintegration01/oracleidentitycloudservice/andrew.gregory@oracle.com <test@example.com>
2022-01-19 13:32:20 -0500

    prefix

--- main.tf
+++ main.tf
@@ -3,5 +3,6 @@ module "vcn" {
   version = "3.2.0"
   # insert required variables here
   compartment_id = var.compartment_id
-  display_name   = var.label_prefix == "none" ? var.vcn_name : "${var.label_prefix}-${var.vcn_name}"
+  vcn_name   = var.label_prefix == "none" ? var.vcn_name : "${var.label_prefix}-${var.vcn_name}"
+  label_prefix = var.label_prefix == "none" ? "" : var.label_prefix
 }
