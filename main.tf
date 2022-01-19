bf80c5e55d80482a9ca0880849c07adedf5bbab9
orasenatdpltintegration01/oracleidentitycloudservice/andrew.gregory@oracle.com <test@example.com>
2022-01-19 13:39:32 -0500

    Update VCN attributes

--- main.tf
+++ main.tf
@@ -5,4 +5,8 @@ module "vcn" {
   compartment_id = var.compartment_id
   vcn_name   = var.vcn_name
   label_prefix = var.label_prefix == "none" ? "" : var.label_prefix
+  create_internet_gateway = true
+  create_nat_gateway = true
+  create_service_gateway = true
+  vcn_cidrs = var.vcn_cidrs
 }
