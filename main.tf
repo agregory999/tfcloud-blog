08e3f9721bfede1886d93a99b6449d58cb180e69
orasenatdpltintegration01/oracleidentitycloudservice/andrew.gregory@oracle.com <test@example.com>
2022-01-19 13:26:06 -0500

    VCN Name

--- main.tf
+++ main.tf
@@ -3,4 +3,5 @@ module "vcn" {
   version = "3.2.0"
   # insert required variables here
   compartment_id = var.compartment_id
+  display_name   = var.label_prefix == "none" ? var.vcn_name : "${var.label_prefix}-${var.vcn_name}"
 }
